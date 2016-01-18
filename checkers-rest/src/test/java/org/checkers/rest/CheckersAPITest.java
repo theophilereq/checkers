60c3b9fa9a8b96488d1a2055fb0a7e1f35ccab6f
theophilereq <theophile.requillart@orange.fr>
2016-01-18 15:10:15 +0100

    Add test for the API

---
+++ checkers-rest/src/test/java/org/checkers/rest/CheckersAPITest.java
@@ -0,0 +1,29 @@
+package org.checkers.rest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+
+import org.checkers.jpa.CheckersAdapter;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+public class CheckersAPITest extends JerseyTest{
+	
+    @Override
+    protected Application configure() {
+        return new CheckersApplication();
+    }
+
+    @Test
+    @Ignore
+    public void itCanCreateAGame() throws Exception{
+    	CheckersAdapter game = target("api").request(MediaType.APPLICATION_JSON)
+    			.get(CheckersAdapter.class);
+    	
+    	assertThat(game.getToken()).isNotNull();
+    }
+}
